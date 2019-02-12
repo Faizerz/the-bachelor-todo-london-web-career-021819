11ad3a48328b126812d53a05b4941d9d3280a6e7
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-12 11:28:55 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -13,8 +13,8 @@ def get_contestant_name(data, occupation)
     if person_hash["occupation"] == occupation
       return person_hash["name"]
     end
+   end
   end
-end
 end
 
 def count_contestants_by_hometown(data, hometown)
