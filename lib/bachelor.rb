4b36776fa8b7e184f1a0181386c1fc5fa171afc4
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-12 11:15:39 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,7 +2,11 @@ def get_first_name_of_season_winner(data, season)
   data.each do |seasons, season_hash|
     if seasons == season
       if season_hash["status"] == "Winner"
-        
+         name_array = season_hash["name"].split
+         return name_array[0]
+       end
+     end
+   end
 end
 
 def get_contestant_name(data, occupation)
